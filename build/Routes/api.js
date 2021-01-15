a53e1ceb18430a94807bbc745ffd186b8f8e769f
Gürkan Şahin <60393263+liilamper@users.noreply.github.com>
2021-01-15 06:21:37 +0300

    Delete build files.

--- build/Routes/api.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("../main");
-main_1.app.get("/accounts", function (req, res) {
-    res.status(200).send("naber");
-});
